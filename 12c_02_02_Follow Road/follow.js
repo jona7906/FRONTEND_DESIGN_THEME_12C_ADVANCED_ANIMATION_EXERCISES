d77464314988af04c35285217fc14d1dad120040
JonasRossen <jona7906@stud.kea.dk>
2021-10-06 00:33:29 +0200

    follow road, insertet svg

---
+++ 12c_02_02_Follow Road/follow.js
@@ -0,0 +1,25 @@
+window.addEventListener("DOMContentLoaded", loadSVG);
+
+function loadSVG() {
+  console.log("load the SVG");
+
+  fetch("car_and_road.svg")
+    .then((response) => response.text())
+    .then((svgData) => {
+      console.log("SVG loaded");
+      document.querySelector("#thesvg").innerHTML = svgData;
+      // TODO: put the SVG into the DOM
+
+      // TODO: Start the animation
+    });
+}
+
+let car = null;
+let curve = null;
+let currentPosition;
+
+function runAnimation() {
+  console.log("animate");
+
+  // TODO: Build animation ...
+}
