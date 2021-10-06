1b17dbc27cddd5d49ee4c58b3b88a16248f02796
JonasRossen <jona7906@stud.kea.dk>
2021-10-06 17:37:47 +0200

    varying keyframes

--- webanimations - exercises/4_more_keyframes.js
+++ webanimations - exercises/4_more_keyframes.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const keyframes = [{ transform: "translate(42vw, 0vw)" }, { transform: "translate(85vw, 20vw)" }, { transform: "translate(42vw, 41vw)" }, { transform: "translate(0vw, 20vw)" }, { transform: "translate(42vw, 0vw)" }];
+const properties = {
+  duration: 5000,
+  iterations: Infinity,
+
+  easing: "ease",
+};
+
+const ball = document.querySelector("#ball");
+const animation = ball.animate(keyframes, properties);
